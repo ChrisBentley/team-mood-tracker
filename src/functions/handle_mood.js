1ea31a7b7cc5f2f7ce01963227c8bbeff89534d8
ChrisBentley <c.j.bentley90@gmail.com>
2019-01-06 23:49:36 +0000

    Added ability to update a mongoDb

--- src/functions/handle_mood.js
+++ src/functions/handle_mood.js
@@ -2,18 +2,61 @@ const dbUrl = process.env.DB_URL;
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
 
+const moment = require('moment');
+var MongoClient = require('mongodb').MongoClient;
+
 function validateValue(moodValue) {
 	let acceptableMoodValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-	return moodValue in acceptableMoodValues;
+	return acceptableMoodValues.indexOf(moodValue) > -1;
 }
 
 function checkInLocalList(userId) {
 	let list = []; // load this from localDb
-	return userId in list;
+	return list.indexOf(userId) > -1;
 }
 
+// function createCollection() {
+// 	MongoClient.connect(dbUrl, function(err, db) {
+// 		if (err) throw err;
+// 		var dbo = db.db("moodsdb");
+
+// 		if (!dbo.collection.getCollection('moods').exists()) {
+// 			db.collection.createCollection("moods", function(err, res) {
+// 				if (err) {
+// 					throw err
+// 				} else {
+// 					console.log("'moods' collection created");
+// 				}
+// 				db.close();
+// 			});
+// 		}
+// 	});
+// }
+
 function saveMoodToDb(moodValue) {
-	// Do DB stuff
+	let today = moment().format('DD/MM/YYYY');
+	let key = {'day':today};
+	let data = {$inc: {[moodValue]:1}};
+
+	MongoClient.connect(dbUrl, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db('moodsdb');
+
+	 	dbo.collection('moods').updateOne(
+			key,
+			data,
+			{upsert: true, safe: false},
+			function(err,data) {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log("Db update succeded");
+				}
+			}
+		);
+
+		db.close();
+	});
 }
 
 function saveUserIdToLocalList(userId) {
